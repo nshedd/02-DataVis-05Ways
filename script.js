c905bb0af00b3d8932cd5cd6297924a475844b74
nshedd <58157630+nshedd@users.noreply.github.com>
2021-02-07 21:37:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 
-// Read the data
-d3.csv('cars-sample.csv', function (data) {
-  // set the dimensions and margins of the graph
-  var margin = { top: 100, right: 100, bottom: 100, left: 100 }
-  var h = 1000 - margin.top - margin.bottom
-  var w = 1000 - margin.left - margin.right
+<script>
 
-  // append the svg object to the body of the page
-  var svg = d3.select('body')
-    .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv", function(data) {
   
   // Add X axis
   var x = d3.scaleLinear()
@@ -42,4 +45,6 @@ d3.csv('cars-sample.csv', function (data) {
       .style("fill", "#69b3a2")
 
 })
+
+</script>
 
