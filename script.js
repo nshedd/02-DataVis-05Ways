45e1d4a3f672666d3757a6d7db3bb71186b15164
nshedd <58157630+nshedd@users.noreply.github.com>
2021-02-07 21:25:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var h = 1000 - margin.top - margin.bottom
 var w = 1000 - margin.left - margin.right
 
 // append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
+var svg = d3.select('body')
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -29,4 +29,17 @@ d3.csv('cars-sample.csv', function (data) {
     .range([ height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y));
-}
+  
+  // Add dots
+  svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x(d.GrLivArea); } )
+      .attr("cy", function (d) { return y(d.SalePrice); } )
+      .attr("r", 1.5)
+      .style("fill", "#69b3a2")
+
+})
+
