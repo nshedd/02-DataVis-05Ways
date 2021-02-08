e266879545df0e82676d5e57b4869985230d16a1
nshedd <58157630+nshedd@users.noreply.github.com>
2021-02-07 21:01:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 d3.csv('cars-sample.csv', function (data) {
   // Variables
   var body = d3.select('body')
-	var margin = { top: 50, right: 50, bottom: 50, left: 50 }
-	var h = 500 - margin.top - margin.bottom
-	var w = 500 - margin.left - margin.right
+	var margin = { top: 100, right: 100, bottom: 100, left: 100 }
+	var h = 1000 - margin.top - margin.bottom
+	var w = 1000 - margin.left - margin.right
 	var formatPercent = d3.format('.2%')
 	// Scales
   var colorScale = d3.scale.category20()
