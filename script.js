a4df707f51ca592cd2f864e6dc49cf872f406960
nshedd <58157630+nshedd@users.noreply.github.com>
2021-02-07 21:31:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-// set the dimensions and margins of the graph
-var margin = { top: 100, right: 100, bottom: 100, left: 100 }
-var h = 1000 - margin.top - margin.bottom
-var w = 1000 - margin.left - margin.right
-
-// append the svg object to the body of the page
-var svg = d3.select('cars_vis')
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
 
 // Read the data
 d3.csv('cars-sample.csv', function (data) {
+  // set the dimensions and margins of the graph
+  var margin = { top: 100, right: 100, bottom: 100, left: 100 }
+  var h = 1000 - margin.top - margin.bottom
+  var w = 1000 - margin.left - margin.right
+
+  // append the svg object to the body of the page
+  var svg = d3.select('body')
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
   
   // Add X axis
   var x = d3.scaleLinear()
