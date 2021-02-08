0eb1c7e5ee3d608966c28a0fba8e3c0ecfd39e89
nshedd <58157630+nshedd@users.noreply.github.com>
2021-02-07 22:08:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,10 @@
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+import { select, csv } from 'd3';
 
-// append the svg object to the body of the page
-var svg = d3.select("scatter")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+const svg = select('svg');
 
-//Read the data
-d3.csv("cars-sample.csv", function(data) {
-  
-  // Add X axis
-  var x = d3.scaleLinear()
-    .domain([0, 5000])
-    .range([ 0, width ]);
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+const width = +svg.attr('width');
+const height = +svg.attr('height');
 
-  // Add Y axis
-  var y = d3.scaleLinear()
-    .domain([0, 47])
-    .range([ height, 0]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
-  
-  // Add dots
-  svg.append('g')
-    .selectAll("dot")
-    .data(data)
-    .enter()
-    .append("circle")
-      .attr("cx", function (d) { return x(d.GrLivArea); } )
-      .attr("cy", function (d) { return y(d.SalePrice); } )
-      .attr("r", 1.5)
-      .style("fill", "#69b3a2")
-
-})
-
+csv('cars-sample.csv').then((data) => {
+  console.log(data);
+});
